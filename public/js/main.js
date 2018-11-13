9c4a9530b85c09769d01fe3714c23dc5eb545dd3
Camtendo <ccrockrom@gmail.com>
2018-11-13 11:21:11 -0600

    Reorganization

---
+++ public/js/main.js
@@ -0,0 +1,64 @@
+if (flvjs.isSupported()) {
+    var videoElement = document.getElementById('videoElement');
+    var flvPlayer = flvjs.createPlayer({
+        type: 'flv',
+        isLive: true,
+        url: 'http://localhost:8000/live/live.flv',
+        hasVideo: true,
+        hasAudio: true,
+    });
+    flvPlayer.attachMediaElement(videoElement);
+    flvPlayer.load();
+    flvPlayer.play();
+
+    flvPlayer.on(flvjs.Events.ERROR, () => {
+        console.log('ERROR WAS THROWN!');
+        reloadPlayer();
+    });
+
+    flvPlayer.on(flvjs.Events.LOADING_COMPLETE, () => {
+        console.log('LOADING COMPLETE WAS THROWN!');
+        reloadPlayer();
+    });
+
+    function updateInfo() {
+        var bitrate = flvPlayer._statisticsInfo ? flvPlayer._statisticsInfo.speed * 8 : 0;
+        if (!bitrate) {
+            document.getElementById('stream-info').innerHTML = 'DISCONNECTED';
+            return;
+        }
+
+        var uptime = flvPlayer.currentTime;
+        var width = flvPlayer.mediaInfo && flvPlayer.mediaInfo.width ? flvPlayer.mediaInfo.width : 0;
+        var height = flvPlayer.mediaInfo && flvPlayer.mediaInfo.height ? flvPlayer.mediaInfo.height : 0;
+        var droppedFrames = flvPlayer._statisticsInfo ? flvPlayer._statisticsInfo.droppedFrames : 0;
+        var decodedFrames = flvPlayer._statisticsInfo ? flvPlayer._statisticsInfo.decodedFrames : 1;
+        var frameRenderPercentage = (decodedFrames - droppedFrames) * 100 / (decodedFrames);
+
+        var newText = buildInfoString(width, height, bitrate, uptime, frameRenderPercentage);
+        document.getElementById('stream-info').innerHTML = newText;
+    }
+
+    function buildTimeString(seconds) {
+        var intSecs = Math.round(seconds);
+        var date = new Date(null);
+        date.setSeconds(intSecs);
+        var result = date.toISOString().substr(11, 8);
+        return result;
+    }
+
+    function buildInfoString(width, height, bitrate, uptime, frameRenderPercentage) {
+        return `${width}x${height}p @ ${bitrate.toFixed(2)}kbps | RTMP uptime: ${buildTimeString(uptime)} | Frames: ${frameRenderPercentage.toFixed(1)}%`;
+    }
+
+    function reloadPlayer() {
+        flvPlayer.unload();
+        flvPlayer.detachMediaElement();
+        flvPlayer.attachMediaElement(videoElement);
+        flvPlayer.load();
+        // the play request is a deferred promise that will wait until the source exists again
+        flvPlayer.play();
+    }
+
+    setInterval(updateInfo, 1000);
+}
--- server.js
+++ server.js
@@ -30,18 +30,6 @@ const config = {
       mediaroot: './media',
       allow_origin: '*'
     },
-    /*trans: {
-      ffmpeg: '/ffmpeg/bin/ffmpeg.exe',
-      tasks: [
-        {
-          app: 'live',
-          hls: true,
-          hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
-          dash: true,
-          dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
-        }
-      ]
-    }*/
   };
    
   var nms = new NodeMediaServer(config)
